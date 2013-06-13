261bf9caff5b58e926e48fcb626e32c5f0328c76
amazo <lhjamazo@gmail.com>
2013-06-13 21:36:53 +0800

    Update index.js

--- index.js
+++ index.js
@@ -9,8 +9,10 @@ exports.calc = function(){
 result = []
 
 while(len > 0){
+  
+  len--;
 
-result.push(shuffle([1, 2, 3, 4, 5, 6, 7, 8, 9, 0]));
+  result.push(shuffle([1, 2, 3, 4, 5, 6, 7, 8, 9, 0]));
 
 }
 
