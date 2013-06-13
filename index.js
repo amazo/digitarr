e5a2dbb92f978de88453be320f602568d8e839a3
amazo <lhjamazo@gmail.com>
2013-06-13 21:36:18 +0800

    Create index.js

---
+++ index.js
@@ -0,0 +1,18 @@
+var shuffle = require('shuffle').shuffle
+
+// '10000000'.length
+
+len = 8
+
+exports.calc = function(){
+
+result = []
+
+while(len > 0){
+
+result.push(shuffle([1, 2, 3, 4, 5, 6, 7, 8, 9, 0]));
+
+}
+
+return result;
+}
